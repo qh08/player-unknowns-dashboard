33b2cd8b937b10fb26ab71d2013c0e01b16d671d
qh08 <qihongfrank@foxmail.com>
2018-03-12 22:17:50 +0800

    add clear webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,6 +9,7 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management',
             template:'./index.html'
