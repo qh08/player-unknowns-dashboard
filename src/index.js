189ba2f9e9a2fe3deae2d7ed1f02172ebd9b741d
qh08 <qihongfrank@foxmail.com>
2018-03-12 21:43:20 +0800

    file-loader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './index.css';
+import pic from './1.jpg';
 
 console.log(1);
+
+var img = document.createElement('img');
+img.src = pic;
+
+document.getElementById('div1').appendChild(img);
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
                 'style-loader',
                 'css-loader'
             ]
+        },{
+            test:/\.(png|svg|jpg|gif)$/,
+            use:[
+                'file-loader'
+            ]
         }]
     }
 };
